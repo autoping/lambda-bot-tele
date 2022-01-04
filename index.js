0f3088f891afe24697987466fa273db8a687fc47
achunin <aleksandr.chunin@t-systems.com>
2022-01-04 17:14:10 +0300

    error handling + serverless yaml

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ bot.start((ctx) => {
 
     ctx.reply('Welcome ' + from.first_name);
     if (uid) {
+        //todo link here
         ctx.reply('You are linked to this bot successfuly. Id is ' + uid);
     }
     if (!uid) {
         ctx.reply('Can not link, there is no id');
     }
-
 
 });
 bot.help((ctx) =>
@@ -37,17 +37,21 @@ module.exports.sendMessage = async (event) => {
     const chatId = input.chatId;
     const message = input.message;
 
+    let statusCode = 201;
+    let statusMessage = "success"
+    try {
+        await bot.telegram.sendMessage(chatId, message);
+    } catch (err) {
+        statusCode = err.error_code|400;
+        statusMessage = err.description;
+    }
 
-    let result = await bot.telegram.sendMessage(chatId, message);
-
-    //todo handle errors
-    result.catch(reason => {
-        console.log(reason);
-    });
     return {
-        statusCode: 201,
+        statusCode: statusCode,
         headers: {
             'Access-Control-Allow-Origin': '*',
-        }
+        },
+        body: JSON.stringify(statusMessage)
     };
 };
+
