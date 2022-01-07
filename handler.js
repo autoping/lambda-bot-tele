a68bf183473c1a3ab5c9775612fc5e47944982fb
onlythenaive <ilya.gubarev@gmail.com>
2022-01-07 17:25:31 +0300

    Inbound/outbound messaging re-worked

--- handler.js
+++ handler.js
@@ -5,29 +5,29 @@ const axios = require('axios');
 const token = process.env.TOKEN
 const baseUrl = "https://api.telegram.org/bot" + token;
 
+function send(messageRequest) {
+  try {
+    return await axios.post(baseUrl + "/sendMessage", messageRequest);
+  } catch (err) {
+    console.error("Error occured while sending Telegram message: " + JSON.stringify(err));
+    // TODO: maybe throw further?
+  }
+}
+
 module.exports.receiveMessage = async (event) => {
   console.log("Inbound message received: " + JSON.stringify(event.body));
+  const messageRequest = {
+    chat_id: event.body.message.chat.id,
+    text: JSON.stringify(event.body)
+  };
+  send(messageRequest);
 }
 
 module.exports.sendMessage = async (event) => {
-
   const input = JSON.parse(event.body);
-  const sendMessageRequest = {
+  const messageRequest = {
     chat_id: input.chatId,
     text: input.message
   };
-
-  try {
-    const result = await axios.post(baseUrl + "/sendMessage", sendMessageRequest);
-    return {
-      statusCode: 201,
-      body: result
-    }
-  } catch (err) {
-    console.error("Error occured while sending Telegram message: " + JSON.stringify(err));
-    return {
-      statusCode: 400,
-      body: err.description
-    }
-  }
+  send(messageRequest);
 }
