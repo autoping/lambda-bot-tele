b47f7be69919af8b589ae70a0b41abef8d3a0103
onlythenaive <ilya.gubarev@gmail.com>
2022-01-07 15:51:08 +0300

    Inbound/outbound messaging re-worked

--- handler.js
+++ handler.js
@@ -5,6 +5,10 @@ const axios = require('axios');
 const token = process.env.TOKEN
 const baseUrl = "https://api.telegram.org/bot" + token;
 
+module.exports.receiveMessage = async (event) => {
+  console.log("Inbound message received: " + event.body);
+}
+
 module.exports.sendMessage = async (event) => {
 
   const input = JSON.parse(event.body);
@@ -13,10 +17,8 @@ module.exports.sendMessage = async (event) => {
     text: input.message
   };
 
-  console.log("SendMessage request received: " + JSON.stringify(sendMessageRequest));
-
   try {
-    const result = await axios.post(baseUrl, sendMessageRequest);
+    const result = await axios.post(baseUrl + "/sendMessage", sendMessageRequest);
     return {
       statusCode: 201,
       body: result
